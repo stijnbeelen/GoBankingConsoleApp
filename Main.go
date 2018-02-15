d71cccf0398e8ea3dc9a84b7c571cfe507c5f4bf
Stijn Beelen <stijn.beelen@hotmail.com>
2018-02-15 15:04:15 +0100

    made struct funtion pay with pointer to receiver

--- Main.go
+++ Main.go
@@ -18,7 +18,7 @@ func main() {
 		make(map[int]int),
 	}
 
-	payChan := make(chan BankAccount)
+/*	payChan := make(chan BankAccount)
 	recChan := make(chan BankAccount)
 	returnChan := make(chan BankAccount)
 
@@ -28,7 +28,42 @@ func main() {
 	pay(payChan,recChan, returnChan, 20)
 
 	fmt.Println(<-returnChan)
-	fmt.Println(<-returnChan)
+	fmt.Println(<-returnChan)*/
+
+	accountKoen.pay(&accountStijn, 50)
+
+	fmt.Println(accountStijn, accountKoen)
 
 
+}
+
+type BankAccount struct {
+	accountNumber int
+	holder string
+	balance int
+	history map[int]int
+}
+
+func (bankAccount *BankAccount) withdraw(amount int) {
+	bankAccount.balance -= amount
+}
+
+func (bankAccount *BankAccount) deposit(amount int) {
+	bankAccount.balance +=  amount
+}
+
+func (payer *BankAccount) pay (receiver *BankAccount, amount int) {
+	payer.withdraw(amount)
+	receiver.deposit(amount)
+}
+
+func pay(payChan, recChan, returnChan chan BankAccount, amount int) {
+	payer := <- payChan
+	receiver := <- recChan
+
+	payer.balance = payer.balance-amount
+	receiver.balance = receiver.balance+amount
+
+	returnChan <- payer
+	returnChan <- receiver
 }
