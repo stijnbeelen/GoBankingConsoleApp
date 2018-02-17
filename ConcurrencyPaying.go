9cd05a03ea2ce555accc909e61f747e690d677f1
Stijn Beelen <stijn.beelen@hotmail.com>
2018-02-17 15:16:02 +0100

    depositting & withdrawing money with goroutines & channels, first try

--- ConcurrencyPaying.go
+++ ConcurrencyPaying.go
@@ -6,27 +6,64 @@ import (
 )
 
 func main() {
-	//wg.Add(5)
+
+	//waitgroup
+	//go pay(&accountStijn, &accountKoen, 50, &wg)
+	//go pay(&accountKoen, &accountStijn, 15, &wg)
+	//go deposit(&accountStijn, 5, &wg)
+	//go withdraw(&accountKoen, 35, &wg)
 	//
-	//go accountStijn.pay(&accountKoen, 50, &wg)
-	//go accountKoen.pay(&accountStijn, 15, &wg)
-	//go accountStijn.deposit(5, &wg)
-
-	pay(&accountStijn, &accountKoen, 50, &wg)
-	pay(&accountKoen, &accountStijn, 15, &wg)
-	deposit(&accountStijn, 5, &wg)
-	withdraw(&accountKoen, 35, &wg)
-
-	// -> uitkomst: koen = 100
-	// -> uitkomst: stijn = 70
-
-	wg.Wait()
+	//// -> uitkomst: koen = 100
+	//// -> uitkomst: stijn = 70
+	//
+	//wg.Wait()
 
 	// for using Locks instead of waitgroup
 	//time.Sleep(time.Second)
 
-	fmt.Println(accountStijn)
-	fmt.Println(accountKoen)
+
+	accDepChan := make(chan *BankAccount)		//account deposit chan
+	accWitChan := make(chan *BankAccount)		//account withdraw chan
+
+	amDepChan := make(chan int)					//amount deposit chan
+	amWitChan := make(chan int)					//amount withdraw chan
+
+
+	go deposit(accDepChan, amDepChan)
+	go withdraw(accWitChan, amWitChan)
+
+	accDepChan <- &accountStijn
+	amDepChan <- 50
+	accWitChan <- &accountKoen
+	amWitChan <- 50
+
+	fmt.Println(*<-accDepChan)
+	fmt.Println(*<-accWitChan)
+
+	accDepChan <- &accountStijn
+	amDepChan <- 25
+	accWitChan <- &accountKoen
+	amWitChan <- 25
+
+	fmt.Println(*<-accDepChan)
+	fmt.Println(*<-accWitChan)
+
+}
+
+func deposit(accChan chan *BankAccount, amChan chan int) {
+	for true {
+		ba := <-accChan
+		ba.balance += <-amChan
+		accChan <- ba
+	}
+}
+
+func withdraw(accChan chan *BankAccount, amChan chan int) {
+	for true {
+		ba := <-accChan
+		ba.balance -= <-amChan
+		accChan <- ba
+	}
 }
 
 func pay (payerAccount, receiverAccount *BankAccount, amount int, wg *sync.WaitGroup) {
@@ -34,14 +71,14 @@ func pay (payerAccount, receiverAccount *BankAccount, amount int, wg *sync.WaitG
 	go payerAccount.pay(receiverAccount, amount, wg)
 }
 
-func deposit(account *BankAccount, amount int, wg *sync.WaitGroup) {
-	wg.Add(1)
-	go account.deposit(amount, wg)
-}
+//func deposit(account *BankAccount, amount int, wg *sync.WaitGroup) {
+//	wg.Add(1)
+//	go account.deposit(amount, wg)
+//}
 
-func withdraw(account *BankAccount, amount int, wg *sync.WaitGroup) {
-	wg.Add(1)
-	go account.withdraw(amount, wg)
-}
+//func withdraw(account *BankAccount, amount int, wg *sync.WaitGroup) {
+//	wg.Add(1)
+//	go account.withdraw(amount, wg)
+//}
 
 
