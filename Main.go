5022ec6e5f1036d0ca6a276191d2ce9bb8fcc288
Stijn Beelen <stijn.beelen@hotmail.com>
2018-02-21 17:46:18 +0100

    added a transfer method

--- Main.go
+++ Main.go
@@ -23,9 +23,10 @@ func main() {
 	if accountKoen.holder == username && accountKoen.password == password || accountStijn.holder == username && accountStijn.password == password {
 
 		for {
-			fmt.Println("1) Deposit")
-			fmt.Println("2) Withdraw")
-			fmt.Println("3) Exit")
+			fmt.Println("1) Zet geld op je rekening")
+			fmt.Println("2) Haal geld af")
+			fmt.Println("3) Schrijf over")
+			fmt.Println("4) Sluit af")
 			fmt.Print("Maak uw keuze: ")
 			fmt.Scan(&choice)
 
@@ -33,26 +34,46 @@ func main() {
 			case 1:
 				fmt.Print("Bedrag: ")
 				fmt.Scan(&amount)
+
 				switch username {
 				case "Stijn":
 					accountDepositChannel <- &accountStijn
 				case "Koen":
 					accountDepositChannel <- &accountKoen
 				}
+
 				amountDepositChannel <- amount
 				fmt.Println(accountStijn, accountKoen)
 			case 2:
 				fmt.Print("Bedrag: ")
 				fmt.Scan(&amount)
+
 				switch username {
 				case "Stijn":
 					accountWithdrawChannel <- &accountStijn
 				case "Koen":
 					accountWithdrawChannel <- &accountKoen
 				}
+
 				amountWithdrawChannel <- amount
 				fmt.Println(accountStijn, accountKoen)
 			case 3:
+				fmt.Print("Bedrag: ")
+				fmt.Scan(&amount)
+
+				switch username {
+				case "Stijn":
+					accountWithdrawChannel <- &accountStijn
+					accountDepositChannel <- &accountKoen
+				case "Koen":
+					accountWithdrawChannel <- &accountKoen
+					accountDepositChannel <- &accountStijn
+				}
+
+				amountDepositChannel <- amount
+				amountWithdrawChannel <- amount
+				fmt.Println(accountStijn,accountKoen)
+			case 4:
 				os.Exit(0)
 			}
 
